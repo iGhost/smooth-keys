60fd93298fcaf5348f5450553ba4027ee8032ad2
Gizzat <gizzat@gmail.com>
2013-08-23 11:56:22 +0600

    [middle commit] setting position through setPosition method

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 (function(){
+	this.tickCooldown = 30;
 	this.keys = [];
 	this.canvas = document.getElementById('canvas');
 	this.box = document.getElementById('box');
@@ -12,33 +13,40 @@
 	this.keyDownHandler = function(e) {
 		if (!e) { e = window.event; }
 		self.keys[e.keyCode] = true;
+//		console.log(e.keyCode);
 	};
 
 	this.keyUpHandler = function(e) {
 		if (!e) { e = window.event; }
 		self.keys[e.keyCode] = false;
-//		console.log('Key released [ ' + e.keyCode + ' ]');
+	};
+
+	this.setPosition = function(top, left) {
+//		if (top > 0 && self.position.top + top < self.canvas.style.height && self.position.top + top >= 0) {
+			self.position['top'] += top;
+//		}
+//		if (left > 0 && self.position.left + left < self.canvas.style.width && self.position.left + left >= 0) {
+			self.position['left'] += left;
+//		}
+		this.moveBox();
 	};
 
 	this.keyboardHandler = function() {
 		if (self.keys[27] == true) {
+			console.log('Stopped!');
 			clearInterval(mainLoop);
 		}
-		if (self.keys[38] == true) {
-			self.position['top'] -= self.step;
-			this.moveBox();
+		if (self.keys[38] == true || self.keys[87] == true) {
+			this.setPosition(-self.step, 0);
 		}
-		if (self.keys[40] == true) {
-			self.position['top'] += self.step;
-			this.moveBox();
+		if (self.keys[40] == true || self.keys[83] == true) {
+			this.setPosition(self.step, 0);
 		}
-		if (self.keys[37] == true) {
-			self.position['left'] -= self.step;
-			this.moveBox();
+		if (self.keys[37] == true || self.keys[65] == true) {
+			this.setPosition(0, -self.step);
 		}
-		if (self.keys[39] == true) {
-			self.position['left'] += self.step;
-			this.moveBox();
+		if (self.keys[39] == true || self.keys[68] == true) {
+			this.setPosition(0, self.step);
 		}
 	};
 
@@ -51,9 +59,9 @@
 		this.keyboardHandler();
 	};
 
-	console.log('Started!');
 	document.onkeydown = this.keyDownHandler;
 	document.onkeyup   = this.keyUpHandler;
 
-	mainLoop = setInterval(this.loop, 20);
+	console.log('Started!');
+	mainLoop = setInterval(this.loop, this.tickCooldown);
 }());
